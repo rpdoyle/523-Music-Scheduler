ff13db66f3c724bcc4e1bb754f1e4f05f1d1a8dc
Faith <fcollins@Faith-Collinss-MacBook-Pro.local>
2015-03-24 07:33:00 -0400

    new method for capturing times, working versin of Student parser. no siblings

--- src/ExcelReader.java
+++ src/ExcelReader.java
@@ -20,10 +20,11 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-// TODO: We may eventually need to make a decision regarding whether or not, and how, to handle end times for meetings.
-// We can ignore this for now.
+// TODO: We may eventually need to make a decision regarding whether or not, and how, 
+//to handle end times for meetings.We can ignore this for now.
 
-// TODO: It's not fantastic to have each error message inline in this file. We can rework these later, but they're fine for now.
+// TODO: It's not fantastic to have each error message inline in this file. 
+//We can rework these later, but they're fine for now.
 
 // Excel Reader contains static methods for parsing Room, Student, and Teacher spreadsheet files
 // into ArrayLists of Room, Student, and Teacher objects.
@@ -45,7 +46,8 @@ public class ExcelReader {
 		// Skip the header row in the spreadsheet
 		rowsIterator.next();
 		
-		// Loop through each row of data, creating a Room object with each iteration
+		// Loop through each row of data, creating a Room object with each iteration 
+		
 		while (rowsIterator.hasNext()) { 
 			XSSFRow currentRow = (XSSFRow)rowsIterator.next();
 			
@@ -53,51 +55,7 @@ public class ExcelReader {
 			
 			String[] specialInstrumentsArr = getStringArrayFromCell(currentRow, 2);
 			
-			// Get arrays of strings representing meetings times throughout the week
-			String[] mondayTimesArr = getStringArrayFromCell(currentRow, 3);
-			String[] tuesdayTimesArr = getStringArrayFromCell(currentRow, 4);
-			String[] wednesdayTimesArr = getStringArrayFromCell(currentRow, 5);
-			String[] thursdayTimesArr = getStringArrayFromCell(currentRow, 6);
-			String[] fridayTimesArr = getStringArrayFromCell(currentRow, 7);
-
-			// Convert the arrays of strings to arrays of integers representing
-			// the meeting start times as the number of minutes since 12:00 AM Monday
-			int[] mondayIntTimes = getStartTimeArr(mondayTimesArr, 0);
-			int[] tuesdayIntTimes = getStartTimeArr(tuesdayTimesArr, 1);
-			int[] wednesdayIntTimes = getStartTimeArr(wednesdayTimesArr, 2);
-			int[] thursdayIntTimes = getStartTimeArr(thursdayTimesArr, 3);
-			int[] fridayIntTimes = getStartTimeArr(fridayTimesArr, 4);
-
-			// Calculate the total number of times the room is available
-			int numberOfTimes = mondayIntTimes.length + tuesdayIntTimes.length + 
-								wednesdayIntTimes.length + thursdayIntTimes.length + 
-								fridayIntTimes.length;
-			
-			int[] availableTimes = new int[numberOfTimes];
-			int startIndex = 0;
-			
-			// For every day that has meeting times, copy those times into the availableTimes array
-			System.arraycopy(mondayIntTimes, 0, availableTimes, startIndex, mondayIntTimes.length);
-			
-			if (mondayIntTimes.length > 0) {
-				startIndex += mondayIntTimes.length;
-				System.arraycopy(tuesdayIntTimes, 0, availableTimes, startIndex, tuesdayIntTimes.length);
-			}
-			
-			if (tuesdayIntTimes.length > 0) {
-				startIndex += tuesdayIntTimes.length;
-				System.arraycopy(wednesdayIntTimes, 0, availableTimes, startIndex, wednesdayIntTimes.length);
-			}
-				
-			if (wednesdayIntTimes.length > 0) {
-				startIndex += wednesdayIntTimes.length;
-				System.arraycopy(thursdayIntTimes, 0, availableTimes, startIndex, thursdayIntTimes.length);
-			}
-				
-			if (thursdayIntTimes.length > 0) {
-				startIndex += thursdayIntTimes.length;
-				System.arraycopy(fridayIntTimes, 0, availableTimes, startIndex, fridayIntTimes.length);
-			}
+			int[] availableTimes = getAvailableTimes(3,4, 5, 6,7, currentRow);
 			
 			// Create a new room object with the data we just parsed out of the spreadsheet
 			Room room = new Room(roomName, specialInstrumentsArr, availableTimes);
@@ -106,8 +64,8 @@ public class ExcelReader {
 			// TODO: remove these print statements after showing Dr. Stotts it works
 			System.out.println("Found another room");
 			System.out.println(room.getName());
-			System.out.println(Arrays.toString(room.getSpecialInstruments()));
-			System.out.println(Arrays.toString(room.getAvailableTimes()));
+			//System.out.println(Arrays.toString(room.getSpecialInstruments()));
+			//System.out.println(Arrays.toString(room.getAvailableTimes()));
 		}
 		
 		workbook.close();
@@ -115,7 +73,80 @@ public class ExcelReader {
 	
 	// Open an excel document with Student data, parse the data, and return an ArrayList of Student objects
 	// TODO: implement
-	public static void parseStudentData(String filepath) {
+	public static void parseStudentData(String filepath) throws FileNotFoundException, IOException, InvalidInputFormatException {
+		
+		FileInputStream inputStream = new FileInputStream(filepath);
+		XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+		XSSFSheet sheet = workbook.getSheetAt(0);
+		Iterator<Row> rowsIterator = sheet.rowIterator();
+		
+		ArrayList<Student> students = new ArrayList<Student>();
+		
+		// Skip the header row in the spreadsheet
+		rowsIterator.next();
+		
+		//an id number to keep track of the student
+		int id = 0;
+		
+		while(rowsIterator.hasNext()){
+			
+			XSSFRow currentRow = (XSSFRow)rowsIterator.next();
+			
+			String name = getStringFromCell(currentRow, 1) + " " + getStringFromCell(currentRow,2);
+			
+			String age = getStringFromCell(currentRow,3);
+			
+			String gender = getStringFromCell(currentRow,6);
+			
+			//check to see if this student is returning and wants to have the same teacher
+			String checker = getStringFromCell (currentRow, 13);
+			String returningTeacher;
+			if (checker.equalsIgnoreCase("yes")){
+				returningTeacher = getStringFromCell (currentRow, 12);	
+			}
+			else{
+				returningTeacher = "none";
+			}
+			
+			String instrumentOfReturningStudent = getStringFromCell(currentRow, 15);
+			
+			//stores the instrument preferences in increasing order i.e. index 0 = choice 1, index 1 = choice 2, etc. 
+			String[] instruments = {getStringFromCell(currentRow, 16),getStringFromCell(currentRow, 18), getStringFromCell(currentRow, 20)}; 
+			
+			//stores the years of experience with each instrument, in increasing order
+			
+			String[] instrumentYears = { getStringFromCell(currentRow, 17), getStringFromCell(currentRow, 19), getStringFromCell(currentRow, 21)};
+			
+			String language = getStringFromCell(currentRow, 69);
+			
+			//calls the helper method that captures the available times from the spreadsheet and returns an array with the times in minutes from
+			// 12 a.m Monday
+			int[] availableTimes = getAvailableTimes(72, 73, 74, 75, 76, currentRow);
+			
+			
+			//Create a new student object with the data we just parsed out of the spreadsheet
+			
+			Student student = new Student(id, name, age, gender, returningTeacher, instrumentOfReturningStudent, instruments, instrumentYears, language, availableTimes);
+			students.add(student);
+			
+			// TODO: remove these print statements after showing Dr. Stotts it works
+			System.out.println("Found another student");
+			System.out.println(student.getID());
+			System.out.println(student.getName());
+			System.out.println(student.getAge());
+			System.out.println(student.getGender());
+			System.out.println(student.getReturningTeacher());
+			System.out.println(student.getInstrumentOfReturningStudent());
+			System.out.println(Arrays.toString(student.getInstruments()));
+			System.out.println(Arrays.toString(student.getInstrumentYears()));
+			System.out.println(student.getLanguage());
+			System.out.println(Arrays.toString(student.getAvailableTimes()));
+			
+			//increase the id so the next student has a new id
+			id++;
+		}
+		
+		workbook.close();
 		
 	}
 	
@@ -146,7 +177,7 @@ public class ExcelReader {
 	}
 	
 	// If possible, read the value of a cell at a given row and cell index as a comma separated array of strings
-	private static String[] getStringArrayFromCell(Row row, int index) throws InvalidInputFormatException {
+	public static String[] getStringArrayFromCell(Row row, int index) throws InvalidInputFormatException {
 		// Get the value of the requested cell, or create a blank cell if the requested cell is null (no value)
 		currentCell = row.getCell(index, Row.CREATE_NULL_AS_BLANK);
 		
@@ -225,5 +256,54 @@ public class ExcelReader {
 		}
 		
 		return startTimes;
+	}
+	
+	private static int[] getAvailableTimes(int a, int b, int c, int d, int e, XSSFRow current) throws InvalidInputFormatException{
+		// Get arrays of strings representing meetings times throughout the week
+		String[] mondayTimesArr = getStringArrayFromCell(current, 72);
+		String[] tuesdayTimesArr = getStringArrayFromCell(current, 73);
+		String[] wednesdayTimesArr = getStringArrayFromCell(current, 74);
+		String[] thursdayTimesArr = getStringArrayFromCell(current, 75);
+		String[] fridayTimesArr = getStringArrayFromCell(current, 76);
+
+		// Convert the arrays of strings to arrays of integers representing
+		// the meeting start times as the number of minutes since 12:00 AM Monday
+		int[] mondayIntTimes = getStartTimeArr(mondayTimesArr, 0);
+		int[] tuesdayIntTimes = getStartTimeArr(tuesdayTimesArr, 1);
+		int[] wednesdayIntTimes = getStartTimeArr(wednesdayTimesArr, 2);
+		int[] thursdayIntTimes = getStartTimeArr(thursdayTimesArr, 3);
+		int[] fridayIntTimes = getStartTimeArr(fridayTimesArr, 4);
+
+		// Calculate the total number of times the room is available
+		int numberOfTimes = mondayIntTimes.length + tuesdayIntTimes.length + 
+							wednesdayIntTimes.length + thursdayIntTimes.length + 
+							fridayIntTimes.length;
+		
+		int[] availableTimes = new int[numberOfTimes];
+		int startIndex = 0;
+		
+		// For every day that has meeting times, copy those times into the availableTimes array
+		System.arraycopy(mondayIntTimes, 0, availableTimes, startIndex, mondayIntTimes.length);
+		
+		if (mondayIntTimes.length > 0) {
+			startIndex += mondayIntTimes.length;
+			System.arraycopy(tuesdayIntTimes, 0, availableTimes, startIndex, tuesdayIntTimes.length);
+		}
+		
+		if (tuesdayIntTimes.length > 0) {
+			startIndex += tuesdayIntTimes.length;
+			System.arraycopy(wednesdayIntTimes, 0, availableTimes, startIndex, wednesdayIntTimes.length);
+		}
+			
+		if (wednesdayIntTimes.length > 0) {
+			startIndex += wednesdayIntTimes.length;
+			System.arraycopy(thursdayIntTimes, 0, availableTimes, startIndex, thursdayIntTimes.length);
+		}
+			
+		if (thursdayIntTimes.length > 0) {
+			startIndex += thursdayIntTimes.length;
+			System.arraycopy(fridayIntTimes, 0, availableTimes, startIndex, fridayIntTimes.length);
+		}
+		return availableTimes;
 	}
 }
