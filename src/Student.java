fa9c9589120e47a114e108f7e00f55754600be9e
Faith <fcollins@Faith-Collinss-MacBook-Pro.local>
2015-03-24 07:31:56 -0400

    Student class-1

---
+++ src/Student.java
@@ -0,0 +1,73 @@
+/*
+ * File: Student.java
+ * Description: This class stores data for a Student.
+ */
+public class Student {
+	
+	private int id;
+	private String name;
+	private String age;
+	private String gender;
+	private String returningTeacher;
+	private String instrumentOfReturningStudent;
+	private String[] instruments;
+	private String[] instrumentYears;
+	private String language;
+	private int[] availableTimes;
+	
+	//TODO: incorporate siblings somehow
+	
+	public Student(int id, String name, String age, String gender, String returningTeacher, String instrumentOfReturningStudent, String[] instruments,  
+			String[] instrumentYears, String language, int[] availableTimes){
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.returningTeacher = returningTeacher;
+		this.instrumentOfReturningStudent = instrumentOfReturningStudent;
+		this.instruments = instruments;
+		this.instrumentYears = instrumentYears;
+		this.language = language;
+		this.availableTimes= availableTimes;
+	}
+	
+	public int getID(){
+		return id;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public String getAge(){
+		return age;
+	}
+	
+	public String getGender(){
+		return gender;
+	}
+	
+	public String getReturningTeacher(){
+		return returningTeacher;
+	}
+	
+	public String getInstrumentOfReturningStudent(){
+		return instrumentOfReturningStudent;
+	}
+	
+	public String[] getInstruments(){
+		return instruments;
+	}
+	
+	public String[] getInstrumentYears(){
+		return instrumentYears;
+	}
+	
+	public String getLanguage(){
+		return language;
+	}
+	
+	public int[] getAvailableTimes(){
+		return availableTimes;
+	}
+}
