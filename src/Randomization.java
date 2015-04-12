6f3491e31168b8347ee15ec8f6e372b869cf6394
Alex Parker <alex.parker215@gmail.com>
2015-04-12 19:36:05 -0400

    added more stuff

--- src/Randomization.java
+++ src/Randomization.java
@@ -6,13 +6,16 @@ public class Randomization {
 
 	public static final int SUFFICIENTSCORE = 10000;
 	public static final int MAXTIMESINCEIMPROVEMENT = 500;
+	public static final int MAXTIMESINCENEWPAIR = 500;
 	
 	private ArrayList<Pair> mandatoryPairs;
 	private Pair[][] scores;
+	private ArrayList<Room> rooms;
 	
-	public Randomization(ArrayList<Pair> mandatoryPairs, Pair[][] scores) {
+	public Randomization(ArrayList<Pair> mandatoryPairs, Pair[][] scores, ArrayList<Room> rooms) {
 		this.mandatoryPairs = mandatoryPairs;
 		this.scores = scores;
+		this.rooms = rooms;
 	}
 	
 	public ArrayList<Pair> getMandatoryPairs() {
@@ -23,22 +26,36 @@ public class Randomization {
 		return scores;
 	}
 	
+	public ArrayList<Room> getRooms() {
+		return rooms;
+	}
+	
 	public HungarianResult schedule() {
 		ArrayList<Pair> possiblePairs = new ArrayList<>();
 		possiblePairs.addAll(mandatoryPairs);
-		int maxScore = 0, timeSinceImprovement = 0;
+		int maxScore = 0, timeSinceImprovement = 0, timeSinceNewPair = 0;
 		
 		int numTeachers = scores.length;
 		int numStudents = scores[0].length;
+		int numRoomDayTimes = HelperMethods.getRoomDayTimes(rooms).size();
 		
 		Random randS = new Random();
 		Random randT = new Random();
 		
 		while (maxScore < SUFFICIENTSCORE && timeSinceImprovement < MAXTIMESINCEIMPROVEMENT) {
-			int studentIndex = randS.nextInt() * numStudents;
-			int teacherIndex = randT.nextInt() * numTeachers;
 			
-			
+			while (possiblePairs.size() < numRoomDayTimes + 20 && timeSinceNewPair < MAXTIMESINCENEWPAIR) {
+				int studentIndex = randS.nextInt() * numStudents;
+				int teacherIndex = randT.nextInt() * numTeachers;
+
+				if (scores[teacherIndex][studentIndex].getScore() > 0) {
+					possiblePairs.add(scores[teacherIndex][studentIndex]);
+					
+					timeSinceNewPair = 0;
+				} else {
+					timeSinceNewPair++;
+				}
+			}
 		}
 	}
 }
