0e9d39e129e856d2ee4b53383063e5a19bece777
Alex Parker <alex.parker215@gmail.com>
2015-04-30 22:11:33 -0400

    Removed comments that described each class and method. Also removed the file description at the top of each class. Rather, Javadoc comments were implemented for each class and method.

--- src/PairTime.java
+++ src/PairTime.java
@@ -1,22 +1,43 @@
 /* 
  * File: PairTime.java
- * Description: This class stores a Pair object and the time
- * 				at which that pair is scheduled.
  */
 
+/**
+ * PairTime is the class that encapsulates a Pair object and the time at which
+ * that Pair will be scheduled.
+ *
+ */
 public class PairTime {
 	private Pair pair;
 	private RoomDayTime roomDayTime;
-	
+
+	/**
+	 * Constructor for a PairTime object.
+	 * 
+	 * @param pair
+	 *            the student/teacher pair
+	 * @param roomDayTime
+	 *            the time at which the pair will have lessons
+	 */
 	public PairTime(Pair pair, RoomDayTime roomDayTime) {
 		this.pair = pair;
 		this.roomDayTime = roomDayTime;
 	}
-	
+
+	/**
+	 * Returns the Pair in this object.
+	 * 
+	 * @return the Pair object that will be having lessons
+	 */
 	public Pair getPair() {
 		return pair;
 	}
-	
+
+	/**
+	 * Returns the RoomDayTime object for this lesson.
+	 * 
+	 * @return the RoomDayTime for this lesson
+	 */
 	public RoomDayTime getRoomDayTime() {
 		return roomDayTime;
 	}
--- src/PairTimeComparator.java
+++ src/PairTimeComparator.java
@@ -1,34 +1,58 @@
 /*
  * File: PairTimeComparator.java
- * Description: Implements methods that allow PairTime objects to be compared
  */
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
-public class PairTimeComparator {
-	 
-	public int compare(PairTime pt1, PairTime pt2){
+/**
+ * PairTimeComparator is used to compare two PairTime objects. Since two
+ * PairTime objects may have identical field values but be references to
+ * different objects, this class implements methods to test these objects for
+ * equality.
+ *
+ */
+public class PairTimeComparator implements Comparator<PairTime> {
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
+	 */
+	@Override
+	public int compare(PairTime pt1, PairTime pt2) {
 		PairComparator pc = new PairComparator();
 		RoomDayTimeComparator rdtc = new RoomDayTimeComparator();
-		
+
 		// Check to see if the two pairs are the same
-		if (pc.compare(pt1.getPair(),pt2.getPair()) == 0){
-			
+		if (pc.compare(pt1.getPair(), pt2.getPair()) == 0) {
+
 			// Check to see if the two RoomDayTimes are the same
-			if (rdtc.compare(pt1.getRoomDayTime(), pt2.getRoomDayTime()) == 0){
+			if (rdtc.compare(pt1.getRoomDayTime(), pt2.getRoomDayTime()) == 0) {
 				return 0;
 			}
 		}
 		return 1;
 	}
 
-	public int compareLists(ArrayList<PairTime> pt1, ArrayList<PairTime> pt2){
-		if (pt1.size() != pt2.size()){
+	/**
+	 * Compares two ArrayLists of PairTime objects for equality. This method
+	 * will ensure that, if a PairTime object exists in one ArrayList, a
+	 * PairTime object with identical fields exists in the other ArrayList.
+	 * 
+	 * @param pt1
+	 *            the first PairTime object to be compared
+	 * @param pt2
+	 *            the second PairTime object to be compared
+	 * @return 0 if the ArrayLists are equal; 1 if the ArrayLists are not equal
+	 */
+	public int compareLists(ArrayList<PairTime> pt1, ArrayList<PairTime> pt2) {
+		if (pt1.size() != pt2.size()) {
 			return 1;
 		}
-		
-		for (int i = 0; i<pt1.size(); i++){
-			if (compare(pt1.get(i), pt2.get(i)) == 1){
+
+		for (int i = 0; i < pt1.size(); i++) {
+			if (compare(pt1.get(i), pt2.get(i)) == 1) {
 				return 1;
 			}
 		}
