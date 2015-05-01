102fa3eaaec5cbc07dca03016f99024e7d6ca71e
Ryan Doyle <rpdoyle@outlook.com>
2015-05-01 12:57:57 -0400

    Updated javadoc

--- src/AllTests.java
+++ src/AllTests.java
@@ -10,8 +10,9 @@ import org.junit.runners.Suite.SuiteClasses;
 @SuiteClasses({ ExcelReaderTest.class, ExcelWriterTest.class,
 		HelperMethodsTest.class, HungarianResultTest.class,
 		HungarianTestFaith.class, HungarianTestRyan.class, PairTest.class,
-		PairTimeTest.class, RoomDayTimeTest.class, RoomTest.class,
-		ScoringEngineTest.class, StudentTest.class, TeacherTest.class })
+		PairTimeTest.class, RandomizationTest.class, RoomDayTimeTest.class,
+		RoomTest.class, ScoringEngineTest.class, StudentTest.class,
+		TeacherTest.class })
 public class AllTests {
 
 }
